fb3d82a6fd86f0712f02c4b9487da835f5d67841
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-16 10:35:49 +0600

    bank

---
+++ Js/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // console.log(userEmail);
+    const passwordField = document.getElementById('user-password');
+    const userPass = passwordField.value;
+    console.log(userPass);
+})
