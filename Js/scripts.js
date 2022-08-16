88a1bb599737c53efb576134de98403deb6d3c97
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-16 12:15:50 +0600

    banking site

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,8 +1,14 @@
+
+
 document.getElementById('login-submit').addEventListener('click', function () {
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
     // console.log(userEmail);
     const passwordField = document.getElementById('user-password');
     const userPass = passwordField.value;
-    console.log(userPass);
+    // console.log(userPass);
+
+    if (userEmail == 'baperbank@gmail.com' && userPass == 'bank1234') {
+        window.location.href = 'bank.html';
+    }
 })
